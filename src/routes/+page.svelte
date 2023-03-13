61cb5afea20ad2b50c4d4cc1550101224bbbabe4
plaine1226 <robinny1226@gmail.com>
2023-03-13 00:47:28 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+	import ChatMessage from '$lib/components/ChatMessage.svelte'
+	import type { ChatCompletionRequestMessage } from 'openai'
+	import { SSE } from 'sse.js'
+
+	let query: string = ''
+	let answer: string = ''
+	let loading: boolean = false
+	let chatMessages: ChatCompletionRequestMessage[] = []
+	let scrollToDiv: HTMLDivElement
+
+	function scrollToBottom() {
+		setTimeout(function () {
+			scrollToDiv.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
+		}, 100)
+	}
+
+	const handleSubmit = async () => {
+		loading = true
+		chatMessages = [...chatMessages, { role: 'user', content: query }]
+
+		const eventSource = new SSE('/api/chat', {
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			payload: JSON.stringify({ messages: chatMessages })
+		})
+
+		query = ''
+
+		eventSource.addEventListener('error', handleError)
+
+		eventSource.addEventListener('message', (e) => {
+			scrollToBottom()
+			try {
+				loading = false
+				if (e.data === '[DONE]') {
+					chatMessages = [...chatMessages, { role: 'assistant', content: answer }]
+					answer = ''
+					return
+				}
+
+				const completionResponse = JSON.parse(e.data)
+				const [{ delta }] = completionResponse.choices
+
+				if (delta.content) {
+					answer = (answer ?? '') + delta.content
+				}
+			} catch (err) {
+				handleError(err)
+			}
+		})
+		eventSource.stream()
+		scrollToBottom()
+	}
+
+	function handleError<T>(err: T) {
+		loading = false
+		query = ''
+		answer = ''
+		console.error(err)
+	}
+
+	
+</script>
+
+<div class="main flex flex-col pt-4 w-full px-8 items-center gap-2">
+	<div class="header">
+		<h1 class="title">Robina</h1>
+		<p class="subtitle">By Robin Hwang</p>
+		<p class="subtitle"><i>A digital companion that's here to listen to your mental health needs</i></p>
+	</div>
+	<div class="chatbox">
+		<div class="flex flex-col gap-2">
+			<ChatMessage type="assistant" message="Hey there! My name is Robina, and I'll be your conselor for today. How can I assist your mental health?" />
+			{#each chatMessages as message}
+				<ChatMessage type={message.role} message={message.content} />
+			{/each}
+			{#if answer}
+				<ChatMessage type="assistant" message={answer} />
+			{/if}
+			{#if loading}
+				<ChatMessage type="assistant" message="Loading.." />
+			{/if}
+		</div>
+		<div class="" bind:this={scrollToDiv} />
+	</div>
+	<form
+		class="formClass flex w-full gap-4 bg-orange-200 p-4"
+		on:submit|preventDefault={() => handleSubmit()}
+	>
+		<input type="text" class="input input-bordered w-full " bind:value={query} placeholder="Start typing here..."/>
+		<button type="submit" class="btn btn-accent bg-orange-300 border-0"> Send </button>
+	</form>
+</div>
